b6f4123791cf432efdee41c5ef025f745415514c
Steve.0 <svilwock@msn.com>
2014-10-18 13:13:59 -0700

    Made changes to goal list

--- frontend/goal_list.js
+++ frontend/goal_list.js
@@ -1,8 +1,10 @@
 $(function() {
+	var userId;
+
 	function getGoals() {
 		$.ajax({
 			dataType: "json",
-			url: "/goal",//username or something.json
+			url: "localhost:3000/user/" + "userID" + "/goal",//username or something.json
 			success: function(result){
 				// append goals to the div in html
 				showGoals(result);
@@ -19,12 +21,43 @@ $(function() {
 	}
 
 	function addGoal() {
-		var goal = $("#add_text").val();
-		$("#goal_list").append("<li>" + goal + "</li>");
+		var goal = "\"text\": " + "\"" + $("#add_text").val() + "\",\n";
+		var obj = jQuery.parseJSON("{" + goal + "}");
+		$.ajax({
+			dataType: "json",
+			url: "localhost:3000/user/" + userID + "/goal",
+			type: "POST",
+			data: obj;
+			success: function(result){
+				// append goal
+				$("#goal_list").append("<li>" + $("#add_text").val() + "</li>");
+				$("#add_text").val() = "";
+			};
+		});
+	}
+
+	function showInfo() {
+		var userName = getCookie("userName");
+		$("user").html(username);
+	}
+
+	function getCookie(cname) {
+	    var name = cname + "=";
+	    var ca = document.cookie.split(';');
+	    for(var i=0; i<ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') c = c.substring(1);
+	        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+	    }
+	    return "";
 	}
 
 	window.onload = function() {
-		getGoals();
-		Document.getElementById("add_button") = addGoal;
+		userId = getCookie("userID");
+	    if (userId != "") {
+			showInfo();
+			getGoals();
+			Document.getElementById("add_button").onclick = addGoal;
+	    }
 	};
 })();
